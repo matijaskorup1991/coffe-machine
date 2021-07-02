00c8639b57856e4b6cbf131af66a872cb4e060da
matijaskorup1991 <smatija1991@gmail.com>
2021-07-02 08:52:04 +0200

    start

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+import data
 
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+def calculate_amount(quarters, dimes, nickles, pennies):
+    quarters_total = quarters * 0.25
+    dimes_total = dimes * 0.10
+    nickles_total = nickles * 0.05
+    pennies_total = pennies * 0.01
+    return quarters_total + dimes_total + nickles_total + pennies_total
+
+
+drink = input("would you like? (espresso/latte/cappuccino): ")
+if drink == "report":
+    print(
+        f"water: {data.resources['water']}\nmilk: {data.resources['milk']}\ncoffe: {data.resources['coffee']}")
+else:
+    quarters = float(input("how many quarters? "))
+    dimes = float(input("how many dimes? "))
+    nickles = float(input("how many nickles? "))
+    pennies = float(input("how many pennies? "))
+
+    if data.MENU[drink]["cost"] > calculate_amount(quarters, dimes, nickles, pennies):
+        print(
+            f"Not enough money, {data.MENU[drink]['cost']} and you have: {calculate_amount(quarters,dimes,nickles,pennies)} Money refunded")
